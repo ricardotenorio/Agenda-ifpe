31f2e5a801318ecc49ebebea0e0f14065ffa19e4
ricardotenorio <ricardo.heber@gmail.com>
2021-09-19 22:46:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgendaTelefones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
